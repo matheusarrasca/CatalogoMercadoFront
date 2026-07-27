8cf2c68eca3b9955578b502e6186bed5d3f6c033
matheusarrasca <matheus.arrasca19@gmail.com>
2026-07-27 20:37:22 -0300

    criando novas pastas

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,11 @@
+function Home() {
+  return (
+    <main>
+      <h2>Bem-vindo ao Catálogo Mercado</h2>
+
+      <p>Sistema de gerenciamento de produtos e categorias.</p>
+    </main>
+  );
+}
+
+export default Home;
